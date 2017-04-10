90a73efd7f79514867e5f972a1f96594d80917e4
Federico <admin@imgdigital.com.ar>
2017-04-10 20:19:43 -0300

    a ver si saco el meta

--- template-parts/content.php
+++ template-parts/content.php
@@ -25,22 +25,20 @@ else :
 endif;
 
 ?>
-<!--
-<?php	if ( 'post' === get_post_type() ) : ?>
-	<div class="entry-meta">
-		<?php turismointer_posted_on();
+
+<?php	//if ( 'post' === get_post_type() ) : ?>
+	<!--<div class="entry-meta">
+		<?php //turismointer_posted_on();
 ?>
 	</div><!-- .entry-meta 
 	<?php
-	endif;
+	//endif;
 ?>
 -->
 	</header><!-- .entry-header -->
 	<div class="entry-content">
 		<?php
 			the_content( sprintf(
-
-
 /* translators: %s: Name of current post. */
 wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'turismointer' ), array( 'span' => array( 'class' => array() ) ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
