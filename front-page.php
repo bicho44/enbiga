142b7f00d99bcf02ff30070d732c65125d6f9556
Federico <admin@imgdigital.com.ar>
2017-04-12 20:51:19 -0300

    platos redondos?

--- front-page.php
+++ front-page.php
@@ -7,7 +7,17 @@
 * @package Filmarte
 */
 
-get_header(); ?>
+get_header(); 
+$opciones_imgd = get_option('opciones_imgd');
+
+$slider = $opciones_imgd['imgd_slider'][0];
+$slider_size = $opciones_imgd['imgd_slider_size'];
+
+?>
+
+<?php if ($slider!=0) {?>
+	<?php include( locate_template( 'template-parts/carrousel.php' ) );?>
+<?php } ?>
 
 <?php
  while ( have_posts() ) : the_post();
