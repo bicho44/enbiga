10a6cf4dcea39cea406b1b23ce441cb986790765
Federico <admin@imgdigital.com.ar>
2017-05-26 20:07:42 -0300

    dos scripts menos

--- functions.php
+++ functions.php
@@ -186,9 +186,9 @@ add_action( 'widgets_init', 'turismointer_widgets_init' );
 function turismointer_scripts() {
 	wp_enqueue_style( 'turismointer-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'turismointer-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	//wp_enqueue_script( 'turismointer-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'turismointer-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	//wp_enqueue_script( 'turismointer-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
