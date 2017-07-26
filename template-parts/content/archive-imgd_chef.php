b821763279c982fce926cb032d3476a0d14461fb
Federico <admin@imgdigital.com.ar>
2017-07-25 23:14:44 -0300

    paises??

--- template-parts/content/archive-imgd_chef.php
+++ template-parts/content/archive-imgd_chef.php
@@ -24,7 +24,7 @@
           the_post_thumbnail($imgd_image_size, $attr);
         ?>
       </a>
-      <?php $args = array('number' => '1',);
+      <?php $args = array('number' => '2',);
             $terms = get_terms('ciudad', $args );
             foreach( $terms as $term ){
               echo __('<strong>País</strong>','imgd'). $term->name ;
